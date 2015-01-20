768886050d7bc3af20ee2134b7dfb3b38c8b7fb7
KarnageKnight <karnageknight@KarnageightsMBP.local.lan>
2015-01-20 21:58:46 +0530

    Initial Commit

---
+++ Bank_acc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bank_acc
+//
+//  Created by KarnageKnight on 20/01/15.
+//  Copyright (c) 2015 KKCPP. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
